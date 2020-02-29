79973ce6dc8df87224187b16cfe8a3fdbdd04a02
Andrew_Programming <andrew.deng@andrew-programming.com>
2020-02-29 12:10:40 +0800

    change class name

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: NinjaCard(),
+      home: ProgrammingLevelCard(),
     ));
 
-class NinjaCard extends StatelessWidget {
+class ProgrammingLevelCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
